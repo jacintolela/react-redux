81634cbd5c92ace8a40f9a54f416ec6a4f1e86bc
jacintolela <mandekejlela@gmail.com>
2021-01-04 11:50:00 -0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      Hello World !
-    </div>
-  );
+class App extends React.Component{
+
+  state = {
+    nome : 'Jacinto Lela'
+  }
+
+  modificarNome = (e) => {
+
+  }
+
+  render(){
+    return(
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome}/>
+        <h1>Hello {this.state.nome} !</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <button>Salvar</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
